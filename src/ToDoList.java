5390d2e057cf007a8c7bd8f7492ef26e2a9cc519
Vithushine <vithushine17@gmail.com>
2025-01-10 11:54:47 -0500

    fixed the error, and made the to do list

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,10 +1,5 @@
-
-/**
- * author: Vithushine 
- * date: 01/09/2025
- * To Do List Maker
- */
-
-public static void main(String[] args) { 
-    System.out.println("Hello, create your To Do List today!");
+public class ToDoList {
+    public static void main(String[] args) {
+        System.out.println("Hello, create your To Do List today!");
+    }
 }
