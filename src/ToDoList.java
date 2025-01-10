fc5792c17f7a756a2c7c4a395bff3c22d0d36bf2
Vithushine <vithushine17@gmail.com>
2025-01-10 11:43:56 -0500

    make to do list class

---
+++ src/ToDoList.java
@@ -0,0 +1,10 @@
+
+/**
+ * author: Vithushine 
+ * date: 01/09/2025
+ * To Do List Maker
+ */
+
+public static void main(String[] args) { 
+    System.out.println("Hello, create your To Do List today!");
+}
