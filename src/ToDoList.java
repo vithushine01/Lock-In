d4fa611ab1c45279f58baaf42f02f7917d706a89
Vithushine <vithushine17@gmail.com>
2025-01-10 12:00:31 -0500

    header comments

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,3 +1,9 @@
+/** 
+ * author: Vithushine
+ * date: 01/09/2025
+ * To Do List Maker
+ */
+
 public class ToDoList {
     public static void main(String[] args) {
         System.out.println("Hello, create your To Do List today!");
